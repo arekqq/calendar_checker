fa92a7701073cc434683d62411eaa4ccfb7b1ca0
Arek <arek.rogacki@gmail.com>
2019-02-27 21:15:58 +0100

    First working version

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,4 +1,4 @@
-import gspread
+import gspread, datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 
@@ -7,15 +7,33 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
-
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
 sheet = client.open("copy_of_plany_dyzurowe")
 
-# Extract and print all of the values
-# list_of_hashes = sheet.get_all_records()
-# print(list_of_hashes)
+now = datetime.datetime.now()
+polishMonths = ["", "STYCZEN", "LUTY", "MARZEC", "KWIECIEN", "CZERWIEC", "LIPIEC", "SIERPIEN", "WRZESIEN", "PAZDZIERNIK", "LISTOPAD", "GRUDZIEN"]
+sheetName = "%s %s" %(polishMonths[now.month], now.year)
+worksheet = sheet.worksheet(sheetName)
 
-worksheet = sheet.worksheet("MARZEC 2019")
-val = worksheet.acell("AI7").value
+startCell = worksheet.find("PLAN")
+print("Found something at R%sC%s" % (startCell.row, startCell.col))
+startRow = startCell.row + 5
+startCol = startCell.col + 1
+shifts = [None] * 32
+for j in range(3):
+	for i in range(32):
+		if j == 0 and worksheet.cell(startRow + i, startCol + j).value == "AR":
+			shifts[i+1] = 1
+			print(str(i + 1) + ": \t 1")
+		if j == 1 and worksheet.cell(startRow + i, startCol + j).value == "AR":
+			shifts[i+1] = 2
+			print(str(i + 1) + ": \t 2")
+		if j == 2 and worksheet.cell(startRow + i, startCol + j).value == "AR":
+			shifts[i+1] = 3
+			print(str(i + 1) + ": \t 3")	
+
+
+
+print(shifts)
+
+val = worksheet.cell(startRow, startCol).value
 print(val)
